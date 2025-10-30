2f70f0bbafb79af6c7cfd8d7d7a81179ddb6749a
devbysaad <dev.bysaad@gmail.com>
2025-10-30 21:20:03 +0500

    cors update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://moodytyunes.vercel.app/", // ✅ Add your Vercel frontend
+      "https://moodytyunes.vercel.app", // ✅ Vercel frontend (removed trailing slash)
     ],
     credentials: true,
   })
